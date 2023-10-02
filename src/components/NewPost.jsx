3f39429ea4d2e864b92262e3a9319e9178c10e9e
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-10-02 22:17:42 +0300

    Add eventListener to NewPost

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,30 @@
 import classes from './NewPost.module.css';
+import { useState } from 'react';
 
 function NewPost() {
+    const [currentState, setCurrentState] = useState();
+
+    
+
+    const changeBodyHandle = (event) => {
+        event.target.value(setCurrentState);
+    };
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea 
+            id="body" 
+            onChange={changeBodyHandle}
+            required rows={3} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input 
+            type="text" 
+            
+            id="name" required />
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,17 @@
+import NewPost from './NewPost';
 import Post from './Post';
 import styles from './PostsList.module.css';
 
 const PostsList = () => {
     return (
-        <ul className={styles.posts}>
-            <Post author='Natali' body='React JS is awesome!' />
-            <Post author='Semih' body='I want to learn React' />
-            <Post author='Buddy' body='Hey guys,are there React lovers there?' />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={styles.posts}>
+                <Post author='Natali' body='React JS is awesome!' />
+                <Post author='Semih' body='I want to learn React' />
+                <Post author='Buddy' body='Hey guys,are there React lovers there?' />
+            </ul>
+        </>
     );
 }
 
