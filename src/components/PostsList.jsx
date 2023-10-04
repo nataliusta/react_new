68d02bf8e09f7bc0f51f5b0f6313949be145fc9f
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-10-04 23:16:10 +0300

    Add loading

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,12 +7,15 @@ import styles from './PostsList.module.css';
 
 const PostsList = ({isPosting, onStopPosting}) => {
     const [posts, setPosts] = useState([]);
+    const [isFetching, setIsFetching] = useState(false);
 
     useEffect(() => {
         async function fetchPosts() {
+            setIsFetching(true);
             const response = await fetch('https://react-new-a4be5-default-rtdb.europe-west1.firebasedatabase.app/posts');
             const resData = await response.json();
             setPosts(resData.posts);
+            setIsFetching(false);
         }
 
         fetchPosts();
@@ -39,17 +42,21 @@ const PostsList = ({isPosting, onStopPosting}) => {
                     />
                 </Modal>
             }
-            {posts.length > 0 && (
+            {!isFetching && posts.length > 0 && (
                 <ul className={styles.posts}>
                 {posts.map((post) => <Post key={post.body} body={post.body} name={post.name} />)}
             </ul>
             )}
-            {posts.length === 0 && <div style={{textAlign: 'center', color: 'white'}}>
+            {!isFetching && posts.length === 0 && <div style={{textAlign: 'center', color: 'white'}}>
                 <h2>There are no posts yet..</h2>
                 <p>Start adding some!</p>
                 </div>
             }
-            
+            {isFetching && (
+                <div style={{textAlign: 'center', color: 'white'}}>
+                    <p>Loading...</p>
+                </div>
+            )}
         </>
     );
 }
