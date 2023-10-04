8db7a62674fd617d46d7b4a20c89aea8e677403d
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-10-04 12:42:05 +0300

    Add form submission

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,30 +1,48 @@
+import { useState } from 'react';
 import styles from './NewPost.module.css';
 
 function NewPost(props) {
+  const [enteredBody, setEnteredBody] = useState('');
+    const [enteredName, setEnteredName] = useState('');
+    
+
+    const onBodyChangeHandler = (event) => {
+        setEnteredBody(event.target.value);
+    };
+
+    const onNameChangeHandler = (event) => {
+        setEnteredName(event.target.value);
+    };
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
-  };
+    const postData = {
+      body: enteredBody,
+      name: enteredName
+    };
+    console.log(postData);
+    props.onCancel();
+  }
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={onSubmitHandler}>
       <p>
         <label htmlFor="body">Text</label>
         <textarea 
             id="body" 
-            onChange={props.onBodyChange}
+            onChange={onBodyChangeHandler}
             required rows={3} />
       </p>
       <p>
-        <label htmlFor="name">Your name</label>
+        <label htmlFor="name">Name</label>
         <input 
             type="text" 
-            onChange={props.onNameChange}
+            onChange={onNameChangeHandler}
             id="name" required />
       </p>
       <p className={styles.actions}>
         <button type='button' onClick={props.onCancel}>Cancel</button>
-        <button onSubmit={onSubmitHandler}>Submit</button>
+        <button>Submit</button>
       </p>
     </form>
   );
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,34 +1,17 @@
-import { useState } from 'react';
-
 import NewPost from './NewPost';
 import Post from './Post';
 import Modal from './Modal';
 import styles from './PostsList.module.css';
 
 const PostsList = ({isPosting, onStopPosting, onCancel}) => {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredName, setEnteredName] = useState('');
-    
-
-    const onBodyChangeHandler = (event) => {
-        setEnteredBody(event.target.value);
-    };
-
-    const onNameChangeHandler = (event) => {
-        setEnteredName(event.target.value);
-    };
 
     return (
         <>
             {isPosting && <Modal onClose={onStopPosting} >
-                <NewPost 
-                    onBodyChange={onBodyChangeHandler} 
-                    onNameChange={onNameChangeHandler} 
-                    onCancel={onStopPosting}
+                <NewPost onCancel={onStopPosting}
                 />
             </Modal>}
             <ul className={styles.posts}>
-                <Post author={enteredName} body={enteredBody} />
                 <Post author='Semih' body='I want to learn React' />
                 <Post author='Buddy' body='Hey guys,are there React lovers there?' />
             </ul>
