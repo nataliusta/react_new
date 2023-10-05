9170052882a69c4b6638f7fec51aedf09277ff7b
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-10-05 19:04:26 +0300

    Add RouterProvider to App

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from '.react-router-dom';
+import App from './App';
+import './index.css';
+
+const router = createBrowserRouter([ // array of all routers you have
+  { path: '/', element: <App /> }, // main domain
+
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
